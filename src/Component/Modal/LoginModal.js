6c4eb96213a80e318d2f523f68b54685b42b2054
rakibulanas3935 <anas15-3935@diu.edu.bd>
2021-12-15 01:52:25 +0600

    add from

--- src/Component/Modal/LoginModal.js
+++ src/Component/Modal/LoginModal.js
@@ -11,8 +11,8 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "90%",
-  height: "90%",
+  width: "70%",
+  hight: "84%",
   bgcolor: "#242424",
 
   boxShadow: 24,
@@ -49,102 +49,93 @@ const LoginModal = ({ openbtn, closebtn, open }) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style} className="mb-5">
-          <div className="cross-icon" onClick={closebtn}>
+          <div className="cross-icon mb-5" onClick={closebtn}>
             <i class="fas fa-times"></i>
           </div>
-          <div className="btn-modal d-flex flex-lg-row flex-column align-items-lg-start align-items-center justify-content-center">
-            {/* <div className="guest-btn-modal">
-              <button
-                className="openPayment me-lg-5 me-0"
-                onClick={displayRazorpay}
-              >
-                Continue as Guest
-              </button>
-            </div> */}
-            <div className="login-btn-modal ms-0 mt-5 mt-lg-0 ms-lg-5">
-              <div className=" d-flex justify-content-center">
-                {/* this is for display info */}
-                <button className="openPayment mb-5">User Info</button>
+
+          {/* from  */}
+          <form className="mt-5 mb-5" onSubmit={handleClick}>
+            <div className="container">
+              <div className="row from">
+                <div className="form-group form-white col-md-6">
+                  <label for="inputFirstname">First Name</label>
+                  <input
+                    type="text"
+                    name="firstname"
+                    required
+                    onBlur={handleOnChange}
+                    className="form-control from-input text-white"
+                    id="inputFirstname"
+                    placeholder="First name"
+                  />
+                </div>
+                <div className="form-group  col-md-6">
+                  <label for="inputLastname">Last Name</label>
+                  <input
+                    type="text"
+                    name="lastname"
+                    className="form-control from-input text-white"
+                    id="inputLastname"
+                    placeholder="Last name"
+                    required
+                    onBlur={handleOnChange}
+                  />
+                </div>
               </div>
-              <form onSubmit={handleClick}>
-                <div
-                  className="show-extra d-flex flex-lg-row flex-column align-items-lg-center align-items-center"
-                  style={{ gap: "5px" }}
-                >
-                  <div className="btn-yourname ">
-                    {/* this is input */}
-                    <input
-                      type="text"
-                      className="input-field"
-                      placeholder="Your name"
-                      name="YourName"
-                      required
-                      onBlur={handleOnChange}
-                    />
-                    <i class="fas fa-angle-right"></i>
-                  </div>
-                  {/* this is input */}
+              <div className="form-group">
+                <label for="inputPhone">Phone</label>
+                <input
+                  type="text"
+                  className="form-control from-input text-white"
+                  id="inputPhone"
+                  name="phone"
+                  placeholder="Phone number"
+                  required
+                  onBlur={handleOnChange}
+                />
+              </div>
+              <div className="form-group">
+                <label for="inputEmail">Email</label>
+                <input
+                  type="email"
+                  className="form-control from-input text-white"
+                  id="inputEmail"
+                  name="email"
+                  placeholder="Email address"
+                  required
+                  onBlur={handleOnChange}
+                />
+              </div>
+              <div className="btn-width text-center mx-auto">
+                <button type="submit" className="btn text-center btn-primary">
+                  Get OTP
+                </button>
+              </div>
 
-                  <div className="btn-yourname ">
-                    <input
-                      type="text"
-                      className="input-field"
-                      placeholder="Phone Number"
-                      name="mobile"
-                      required
-                      onBlur={handleOnChange}
-                    />
-                    <i class="fas fa-angle-right"></i>
-                  </div>
-                  {/* this is input */}
-
-                  <div className="btn-yourname ">
-                    <input
-                      type="email   "
-                      className="input-field"
-                      placeholder="Email adress"
-                      name="email"
-                      required
-                      onBlur={handleOnChange}
-                    />
-                    <i class="fas fa-angle-right"></i>
-                  </div>
-
-                  {/* This is the button  */}
-                  <div className="btn-yourname ">
-                    <button className="yourname" type="submit">
-                      Send Otp
-                    </button>
-                  </div>
+              <div className="row align-items-center">
+                <div className="form-group col-md-9">
+                  <label for="inputotp">Enter OTP</label>
+                  <input
+                    type="text"
+                    name="otp"
+                    className="form-control from-input text-white"
+                    id="inputotp"
+                    placeholder="Enter OTP"
+                    required
+                    onBlur={handleOnChange}
+                  />
                 </div>
-
-                <div
-                  className=" d-flex flex-lg-row flex-column align-items-lg-center align-items-center"
-                  style={{ gap: "5px", marginTop: "50px" }}
-                >
-                  <div className="btn-yourname ">
-                    {/* this is input */}
-                    <input
-                      type="text"
-                      className="input-field"
-                      placeholder="Enter otp"
-                      name="otp"
-                      required
-                      onBlur={handleOnChange}
-                    />
-                    <i class="fas fa-angle-right"></i>
-                  </div>
-                  {/* This is the button */}
-                  <div
-                    className="btn-yourname "
-                    onClick={() => displayRazorpay({})}
+                <div className="col-md-3 mx-auto verify mt-3">
+                  <button
+                    type="submit"
+                    className="btn w-100 text-center  btn-primary"
                   >
-                    <button className="yourname">Verify</button>
-                  </div>
+                    Verify
+                  </button>
                 </div>
-              </form>
+              </div>
             </div>
-          </div>
+          </form>
         </Box>
       </Modal>
       <Payment open1={open1} openModal={handleOpen} closeModal={handleClose} />
